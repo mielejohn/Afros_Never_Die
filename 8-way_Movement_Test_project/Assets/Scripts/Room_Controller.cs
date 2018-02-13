af0d303dfa2fe7dd83429da0deae8b4d574e49c8
John_Miele <john_miele@bloomfield.edu>
2018-02-13 18:22:37 -0500

    Day before Prototype 2 Commit, this includes, room alerts and fixed enemy movements

---
+++ 8-way_Movement_Test_project/Assets/Scripts/Room_Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room_Controller : MonoBehaviour {
+
+	public GameObject[] Enemies;
+	public GameObject[] Cameras;
+
+	public GameObject Player;
+	public PlayerMovement PM;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("Something just Collided");
+		if (other.tag == "Player") {
+			Player = other.gameObject;
+			PM = Player.GetComponent<PlayerMovement> ();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.tag == "Player") {
+			ResetRoom ();
+		}
+	}
+
+	public void RoomAlert(){
+		for (int i = 0; i < Enemies.Length; i++) {
+			//Enemies [i].GetComponent<Basic_Enemy> ().PlayerofInterest = Player;
+			Enemies [i].GetComponent<Basic_Enemy> ().DetectedPlayer ();
+		}
+	}
+
+	public void ResetRoom(){
+		Debug.Log ("Called reset room");
+		for (int i = 0; i < Enemies.Length; i++) {
+			Debug.Log ("Calling enemy " + Enemies[i].gameObject.name);
+			Debug.Log ("Called reset room enemy [" + i + "]");
+			//Enemies [i].GetComponent<Basic_Enemy> ().PlayerofInterest = Player;
+			StartCoroutine(Enemies [i].GetComponent<Basic_Enemy> ().ResetGaurd(1.0f));
+		}
+	}
+}
